224683b2765c411c2cbc647c0fcfb98937c18c71
Sumit Kumar <sumitkumarofficial2122@gmail.com>
2023-12-07 15:53:08 +0530

    complete live assignment of 03/12

--- live_assignment/week1.2/02.js
+++ live_assignment/week1.2/02.js
@@ -1,1 +1,6 @@
 // calculate the time it takes between a setTimeout call and the inner function actually running
+setTimeout(timetake, 1000)
+
+function timetake() {
+    console.log("hello world")
+}
--- live_assignment/week1.2/03.js
+++ live_assignment/week1.2/03.js
@@ -1,1 +1,10 @@
 // create a terminal clock(HH:MM:SS)
+
+function terminalClock() {
+    let time = new Date();
+    console.log(time.toLocaleTimeString());
+    setTimeout(terminalClock, 1000);
+}
+terminalClock();
+
+
