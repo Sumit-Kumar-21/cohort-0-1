da08b748d7a1c7a5fefabfdd5c20d296418288fa
Sumit Kumar <sumitkumarofficial2122@gmail.com>
2024-02-17 02:21:09 +0530

    some new thing add

--- Assignment_week11/worker-app-1/my-honoApp/src/router/user.ts
+++ Assignment_week11/worker-app-1/my-honoApp/src/router/user.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-import { handlePostPostreq, handleSigninPostreq, handleSignupPostreq, handlegetUserPosts } from "../controller/user";
+import { handlePostById, handlePostDeleteById, handlePostPostreq, handlePutById, handleSigninPostreq, handleSignupPostreq, handlegetUserPosts } from "../controller/user";
 import { authmiddleware } from "../middleware/user";
 
 export const router = new Hono();
@@ -8,5 +8,7 @@ router.post("/signup", handleSignupPostreq)
 router.post("/signin", handleSigninPostreq)
 router.get("/posts", authmiddleware ,handlegetUserPosts)
 router.post("/post", authmiddleware, handlePostPostreq)
-router.get("/post/:id", authmiddleware,)
+router.get("/post/:id", authmiddleware, handlePostById)
+router.put("/post/:id", authmiddleware, handlePutById)
+router.delete("/post/:id", authmiddleware, handlePostDeleteById)
 
